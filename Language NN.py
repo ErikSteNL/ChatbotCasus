9cf9f3b917c032a9119fa300e935b5742ae95536
Erik Stevens <erikstevens79137@gmail.com>
2017-10-25 12:24:28 +0200

    Functies gesplit

--- Language NN.py
+++ Language NN.py
@@ -13,40 +13,6 @@ def sigmoid(x):
 # convert output of sigmoid function to its derivative
 def sigmoid_output_to_derivative(output):
     return output*(1-output)
- 
-def clean_up_sentence(sentence):
-    # tokenize the pattern
-    sentence_words = nltk.word_tokenize(sentence)
-    # stem each word
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence_words
-
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-def bow(sentence, words, show_details=False):
-    # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words
-    bag = [0]*len(words)  
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
-                bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
-
-    return(np.array(bag))
-
-def think(sentence, words, show_details=False):
-    x = bow(sentence.lower(), words, show_details)
-    if show_details:
-        print ("sentence:", sentence, "\n bow:", x)
-    # input layer is our bag of words
-    l0 = x
-    # matrix multiplication of input and hidden layer
-    l1 = sigmoid(np.dot(l0, synapse_0))
-    # output layer
-    l2 = sigmoid(np.dot(l1, synapse_1))
-    return l2
 
 def train(X, y, classes=None, words= None , hidden_neurons=10, alpha=1, epochs=50000, dropout=False, dropout_percent=0.5):
 
@@ -221,25 +187,6 @@ def MakeBrainFile():
 MakeBrainFile()
 
 # probability threshold
-ERROR_THRESHOLD = 0.2
+
 # load our calculated synapse values
-synapse_file = 'synapses.json' 
-with open(synapse_file) as data_file: 
-    synapse = json.load(data_file) 
-    synapse_0 = np.asarray(synapse['synapse0']) 
-    synapse_1 = np.asarray(synapse['synapse1'])
 
-def classify(sentence, words, classes, show_details=False):
-    results = think(sentence, words, show_details)
-    print results
-    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD ]
-    print results
-    results.sort(key=lambda x: x[1], reverse=True) 
-    return_results =[[classes[r[0]],r[1]] for r in results]
-    print ("%s \n Taal: %s \n Zekerheid: %s%%" % (sentence, return_results[0][0], return_results[0][1]))
-    return return_results
-
-while True:
-    print("#"*40)
-    tempinput = raw_input("Type een zin:\n")
-    classify(str(tempinput), synapse['words'], synapse['classes'], show_details=False)
--- Training.py
+++ Training.py
@@ -1,32 +1,71 @@
-# 4 classes of training data
+import nltk, os, json
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+import numpy as np
+import time
 
-def GetTrainingData():
-    training_data = []
-    training_data.append({"class":"Nederlands", "sentence":"Hallo, hoe gaat het ermee?"})
-    training_data.append({"class":"Nederlands", "sentence":"Dit is een willekeurige zin om het systeem te leren."})
-    training_data.append({"class":"Nederlands", "sentence":"Mijn favoriete hobby is talen leren."})
-    training_data.append({"class":"Nederlands", "sentence":"Ik hoop dat het goed met je gaat!"})
-    training_data.append({"class":"Nederlands", "sentence":"Ik vind het heel leuk om met jou te praten."})
-    training_data.append({"class":"Nederlands", "sentence":"Wil je eens schrijven hoe jouw dag eruitziet?"})
+def sigmoid(x):
+    output = 1/(1+np.exp(-x))
+    return output
 
-    training_data.append({"class":"Engels", "sentence":"Hello, how are you doing?"})
-    training_data.append({"class":"Engels", "sentence":"This is a random sentence to learn the system."})
-    training_data.append({"class":"Engels", "sentence":"My favorite hobby is learning languages."})
-    training_data.append({"class":"Engels", "sentence":"I hope you are doing well!"})
-    training_data.append({"class":"Engels", "sentence":"I like talking to you."})
-    training_data.append({"class":"Engels", "sentence":"Want to write what your day looks like?"})
+# convert output of sigmoid function to its derivative
+def sigmoid_output_to_derivative(output):
+    return output*(1-output)
+ 
+def clean_up_sentence(sentence):
+    # tokenize the pattern
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
 
-    training_data.append({"class":"Duits", "sentence":"Hallo, wie geht es dir?"})
-    training_data.append({"class":"Duits", "sentence":"Dies ist ein zufalliger Satz, um das System zu lernen."})
-    training_data.append({"class":"Duits", "sentence":"Mein Lieblingshobby ist das Sprachenlernen."})
-    training_data.append({"class":"Duits", "sentence":"Ich hoffe es geht dir gut!"})
-    training_data.append({"class":"Duits", "sentence":"Ich spreche gern mit dir."})
-    training_data.append({"class":"Duits", "sentence":"Willst du schreiben, wie dein Tag aussieht?"})
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
 
-    training_data.append({"class":"Frans", "sentence":"salut, comment ca vas?"})
-    training_data.append({"class":"Frans", "sentence":"Ceci est une phrase aleatoire pour apprendre au systeme"})
-    training_data.append({"class":"Frans", "sentence":"Mon hbby prefere est apprendre des languages"})
-    training_data.append({"class":"Frans", "sentence":"J'espere que tu vas bien!"})
-    training_data.append({"class":"Frans", "sentence":"J'aime bien parler avec toi."})
-    training_data.append({"class":"Frans", "sentence":"Veux tu ecrire comment se passe ta journee"})
-    return training_data
+    return(np.array(bag))
+
+
+def think(sentence, words, show_details=False):
+    x = bow(sentence.lower(), words, show_details)
+    if show_details:
+        print ("sentence:", sentence, "\n bow:", x)
+    # input layer is our bag of words
+    l0 = x
+    # matrix multiplication of input and hidden layer
+    l1 = sigmoid(np.dot(l0, synapse_0))
+    # output layer
+    l2 = sigmoid(np.dot(l1, synapse_1))
+    return l2
+
+ERROR_THRESHOLD = 0.2
+synapse_file = 'synapses.json'
+
+with open(synapse_file) as data_file: 
+    synapse = json.load(data_file) 
+    synapse_0 = np.asarray(synapse['synapse0']) 
+    synapse_1 = np.asarray(synapse['synapse1'])
+    
+def classify(sentence, words, classes, show_details=False):
+    results = think(sentence, words, show_details)
+    print results
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD ]
+    print results
+    results.sort(key=lambda x: x[1], reverse=True) 
+    return_results =[[classes[r[0]],r[1]] for r in results]
+    print ("%s \n Taal: %s \n Zekerheid: %s%%" % (sentence, return_results[0][0], return_results[0][1]))
+    return return_results
+
+while True:
+    print("#"*40)
+    tempinput = raw_input("Type een zin:\n")
+    classify(str(tempinput), synapse['words'], synapse['classes'], show_details=False)

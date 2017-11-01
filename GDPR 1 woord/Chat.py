70798197be1c9d5fdfa93869db5fdbb153334dc8
Milan Laumen <mil_1993@hotmail.com>
2017-11-01 14:48:58 +0100

    Aangepast op nieuwe data

--- GDPR 1 woord/Chat.py
+++ GDPR 1 woord/Chat.py
@@ -1,12 +1,26 @@
-import nltk, os, json, time, Data
+import nltk, os, json, time, Data, random
 import numpy as np
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 # probability threshold
-ERROR_THRESHOLD = 0
+ERROR_THRESHOLD = 0.5 # Onder de 50% stel de vraag opnieuw
+CERTAIN_THRESHOLD = 0.9 # Boven de 90% weet hij het antwoord zeker
 # load our calculated synapse values
 synapse_file = 'brain.json'
+
+# Answers when we don't understand the question
+idk_answer = [
+    "Ik begrijp uw vraag niet helemaal, kunt u de vraag anders stellen a.u.b?",
+    "Die vraag vindt ik onduidelijk.",
+    "Ik snap je vraag niet.",
+    "Kun je misschien je vraag anders tellen?"
+    ]
+
+thx_answer = [
+    "Thank you",
+    "No problem"
+    ]
 
 # compute sigmoid nonlinearity
 def sigmoid(x):
@@ -57,25 +71,40 @@ def OpenFile():
         synapse_0 = np.asarray(synapse['synapse0']) 
         synapse_1 = np.asarray(synapse['synapse1'])
 
-
         print("TrainingData geladen")
         return synapse, synapse_0, synapse_1
     
 def classify(sentence, words, classes, show_details=False):
     results = think(sentence, words, show_details)
+    history = []
     try:
         results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD ]
+        print("Results: %s" % results)
         if len(results) > 0:
             results.sort(key=lambda x: x[1], reverse=True) 
             return_results =[[classes[r[0]],r[1]] for r in results]
             answerToQuestion = Data.GetAnswer(return_results[0][0])
-            print ("\n Vraagnummer: %s \n Zekerheid: %s%%" % (return_results[0][0], return_results[0][1]))
-            print("\n\n answer to question: \n\n ")
-            print(str(answerToQuestion))
+            print("\nVraagnummer: %s \nZekerheid: %s%%" % (return_results[0][0], return_results[0][1]))
+            print("\nAnswer to question:\n")
+            print(answerToQuestion)
+            
+            if return_results[0][1] < CERTAIN_THRESHOLD:
+                time.sleep(1)
+                history.append(sentence)
+                q = raw_input("Did this answer your qeustion? Y/N\n") # code 99
+                if "y" in q.lower() or "yes" in q.lower():
+                    print("Added %s to file %s" % (sentence, "brain.json")) # code 98
+                elif "n" in q.lower() or "no" in q.lower():
+                    new_q = raw_input("Sorry, can you reform your question?\n") # code 97
+                    classify(new_q, synapse['words'], synapse['classes'], show_details=False)
+                    
         else:
-            print("Kan de taal niet met zekerheid bepalen...")
+            idk = random.randint(0, len(idk_answer)-1)
+            print(idk)
+            print(idk_answer[idk])
+            
     except Exception, e:
-        print("Kaput: %s" % e)
+        print("Exception error: %s" % e)
 
 
 synapse, synapse_0, synapse_1 = OpenFile()

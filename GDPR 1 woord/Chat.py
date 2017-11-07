2a9d8375adf42141c3ffcabecfa56aacabc486d2
Erik Stevens <erikstevens79137@gmail.com>
2017-11-07 14:39:57 +0100

    vertalen antwoord, kleine problemen ' é en è tekens

--- GDPR 1 woord/Chat.py
+++ GDPR 1 woord/Chat.py
@@ -1,4 +1,4 @@
-from apiclient.discovery import build 
+from apiclient.discovery import build
 import nltk, os, json, time, Data, random, schedule
 import numpy as np
 import re
@@ -6,8 +6,10 @@ from collections import Counter
 from nltk.stem.lancaster import LancasterStemmer
 
 # probability threshold
-ERROR_THRESHOLD = 0 # Onder de 0% stel de vraag opnieuw
+ERROR_THRESHOLD = 0.5 # Onder de 0% stel de vraag opnieuw
 CERTAIN_THRESHOLD = 0.9 # Boven de 90% is een zeker antwoord
+
+BOTDEBUG = True
 
 # load our calculated synapse values
 synapse_file = 'brain.json'
@@ -22,11 +24,15 @@ def sigmoid_output_to_derivative(output):
     return output*(1-output)
 
 def clean_up_sentence(sentence):
+
     # tokenize the pattern
     sentence_words = nltk.word_tokenize(sentence)
     # tag the words
     tags = nltk.pos_tag(sentence_words, tagset='universal')
-    print("TAGS: %s" % tags)
+    if(BOTDEBUG):
+        print("TAGS: %s" % tags)
+
+
 
     sentence_words = []
 
@@ -35,13 +41,15 @@ def clean_up_sentence(sentence):
             sentence_words.append(str(word[0]))
 
     # stem each word
-    print(sentence_words)
     sentence_words = [word.lower() for word in sentence_words]
-    print(sentence_words)
+    if(BOTDEBUG):
+        print(sentence_words)
+
     return sentence_words
 
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 def bow(sentence, words, show_details=False):
+
     # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
     # bag of words
@@ -51,11 +59,13 @@ def bow(sentence, words, show_details=False):
             if w == s:
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
+                    if(BOTDEBUG):
+                        print ("found in bag: %s" % w)
 
     return(np.array(bag),sentence_words)
 
 def think(sentence, words, show_details=False):
+
     x,keywords = bow(sentence.lower(), words, show_details)
     if show_details:
         print ("sentence:", sentence, "\n bow:", x)
@@ -68,6 +78,7 @@ def think(sentence, words, show_details=False):
     return l2, keywords
 
 def OpenFile():
+
     with open(synapse_file) as data_file:
         global synapse
         global synapse_0
@@ -76,12 +87,13 @@ def OpenFile():
         synapse = json.load(data_file)
         synapse_0 = np.asarray(synapse['synapse0'])
         synapse_1 = np.asarray(synapse['synapse1'])
+        if(BOTDEBUG):
+            print("TrainingData loaded")
 
-        print("TrainingData loaded")
         return synapse, synapse_0, synapse_1
 
-def classify(sentence, words, classes, show_details=False):
-    print("CLASSIFY")
+def classify(sentence, words, classes, detectedLanguageSource, show_details=False):
+
     history = []
     results, keywords = think(sentence, words, show_details)
     try:
@@ -90,13 +102,17 @@ def classify(sentence, words, classes, show_details=False):
         if len(results) > 0:
             results.sort(key=lambda x: x[1], reverse=True)
             return_results =[[classes[r[0]],r[1]] for r in results]
-            answerToQuestion = Data.GetAnswer(return_results[0][0])
-            print("\nClassNumber: %s \nCertainty: %s%%" % (return_results[0][0], return_results[0][1]))
-            print("\nAnswer:\n")
+            answerToQuestion = Data.GetAnswer(return_results[0][0], detectedLanguageSource)
+            if(BOTDEBUG):
+                print("\nClassNumber: %s \nCertainty: %s%%" % (return_results[0][0], return_results[0][1]))
+                print("\nAnswer:\n")
             print(answerToQuestion)
 
+
             if return_results[0][1] < CERTAIN_THRESHOLD:
-                print("NOT SURE")
+                if(BOTDEBUG):
+                    print("NOT SURE")
+
                 ask = True
                 addToHistory = True
 
@@ -107,23 +123,30 @@ def classify(sentence, words, classes, show_details=False):
                         for i in keywords:
                             keys += i + " "
                         history.append(keys)
-                    q = raw_input("%s Y/N\n" % Data.GetAnswer(99)) # code 99
-
+                    q = raw_input("%s Yes/No:   " % Data.GetAnswer(99, detectedLanguageSource)) # code 99
+                    print("\n")
                     if "y" in q.lower() or "yes" in q.lower(): # antwoord op vraag is goed
-                        print("Added new sentences to brain")
-                        with open("Vragen.txt", "a+") as f:
+                        if(BOTDEBUG):
+                            print("Added new sentences to brain")
+
+                        with open("Questions.txt", "a+") as f:
                             for i in history:
-                                print("New question: %s | %s" % (return_results[0][0],i))
+                                if(BOTDEBUG):
+                                    print("New question: %s | %s" % (return_results[0][0],i))
                                 f.writelines([return_results[0][0],":",i,"\n"])
                         ask = False
 
                     elif "n" in q.lower() or "no" in q.lower(): # antwoord op vraag is NIET goed
-                        print("NIET GOED")
-                        opnieuw = raw_input("%s Y/N\n" % Data.GetAnswer(97)) # code 97
+                        if(BOTDEBUG):
+                            print("NIET GOED")
 
+                        opnieuw = raw_input("%s Yes/No   :\n" % Data.GetAnswer(97, detectedLanguageSource)) # code 97
+                        print("\n\n")
                         if "y" in opnieuw:  # user wilt opnieuw proberen
-                            print("OPNIEUW")
-                            sentence = raw_input("%s" % Data.GetAnswer(98)) # code 98
+                            if(BOTDEBUG):
+                                print("OPNIEUW")
+
+                            sentence = raw_input("%s" % Data.GetAnswer(98, detectedLanguageSource)) # code 98
                             #classify(sentence, words, classes, show_details=False)
 
                             results, keywords = think(sentence, words, show_details)
@@ -131,43 +154,57 @@ def classify(sentence, words, classes, show_details=False):
                                 results = [[i,r] for i,r in enumerate(results)] # if r>ERROR_THRESHOLD
                                 results.sort(key=lambda x: x[1], reverse=True)
                                 return_results =[[classes[r[0]],r[1]] for r in results]
-                                answerToQuestion = Data.GetAnswer(return_results[0][0])
-                                print("\nClassNumber: %s \nCertainty: %s%%" % (return_results[0][0], return_results[0][1]))
-                                print("\nAnswer:\n%s" % answerToQuestion)
+                                answerToQuestion = Data.GetAnswer(return_results[0][0], detectedLanguageSource)
+                                if(BOTDEBUG):
+                                    print("\nClassNumber: %s \nCertainty: %s%%" % (return_results[0][0], return_results[0][1]))
+                                print(answerToQuestion)
+
                                 if return_results[0][1] > CERTAIN_THRESHOLD:
                                     addToHistory = False
                                 else:
                                     addToHistory = True
                             else:
-                                print("%s" % Data.GetAnswer(100)) # code 100
+                                print("%s" % Data.GetAnswer(100, detectedLanguageSource)) # code 100
 
                         else: # user wilt NIET opnieuw proberen
-                            print("Okay")
+                            if(BOTDEBUG):
+                                print("Okay")
+
                             ask = False
         else:
-            print("%s\n" % Data.GetAnswer(100)) # code 100
+            if(BOTDEBUG):
+                print("%s\n" % Data.GetAnswer(100, detectedLanguageSource)) # code 100
+
 
     except Exception, e:
-        print("Exception error: %s" % e)
+        if(BOTDEBUG):
+            print("Exception error: %s" % e)
+
 
 
 synapse, synapse_0, synapse_1 = OpenFile()
 
 
 #returns translate sentence
-def g_translate(source):
+def g_translate(source, TranslateTo):
     service = (build('translate', 'v2', developerKey='AIzaSyB0x83parhwESgG8Ig8jhN5ZA34_VVyn8Q'))
-    request = service.translations().list(q=source, target='en')
+    request = service.translations().list(q=source, target=TranslateTo)
     response = request.execute()
-    return response['translations'][0]['translatedText']
+    detectedLanguageSource = response['translations'][0]['detectedSourceLanguage']
+    print(detectedLanguageSource)
+    return response['translations'][0]['translatedText'], detectedLanguageSource
 
 
 schedule.every(6).minutes.do(OpenFile)
 
 while True:
     schedule.run_pending()
-    print("\n"+"#"*40)
-    tempinput = raw_input("Type een zin:\n")
-    tempinput = g_translate(tempinput)
-    print "Engels:", tempinput
-    classify(str(tempinput), synapse['words'], synapse['classes'], show_details=False)
+    if(BOTDEBUG):
+        print("\n"+"#"*40)
+
+    tempinput = raw_input("What is your question?:\n")
+    tempinput, detectedLanguageSource = g_translate(tempinput,'en')
+    if(BOTDEBUG):
+        print "Engels:", tempinput
+
+    classify(str(tempinput), synapse['words'], synapse['classes'], detectedLanguageSource, show_details=False)
--- GDPR 1 woord/Data.py
+++ GDPR 1 woord/Data.py
@@ -1,28 +1,40 @@
 import random
+from apiclient.discovery import build
+from unidecode import unidecode
 #json
 
 def GetTrainingData():
     training_data = []
 
     trainingDataRaw = []
-    with open("Vragen.txt") as f:
+    with open("Questions.txt") as f:
         trainingDataRaw = f.readlines()
     trainingDataRaw = [x.strip("\n") for x in trainingDataRaw]
 
     for line in trainingDataRaw:
+        print(line)
         line = line.split(':',1)
         training_data.append({"class":line[0], "sentence":line[1]})
 
     for line in training_data:
         line['sentence'] = line['sentence'].strip().decode("ascii", "ignore").encode("ascii")
+        print(line['sentence'])
         if line['sentence'] == "":
             continue
-
     return training_data
 
-def GetAnswer(number):
+def g_translate(source, TranslateTo):
+    service = (build('translate', 'v2', developerKey='AIzaSyB0x83parhwESgG8Ig8jhN5ZA34_VVyn8Q'))
+    request = service.translations().list(q=source, target=TranslateTo)
+    response = request.execute()
+    detectedLanguageSource = response['translations'][0]['detectedSourceLanguage']
+    test = unidecode(response['translations'][0]['translatedText'])
+    return test
+
+def GetAnswer(number, detectedLanguageSource):
 
     answers = []
+
 
     with open("Answers.txt") as f:
         answers = f.readlines()
@@ -36,9 +48,11 @@ def GetAnswer(number):
     if len(possibleAnswers) == 0:
         return ""
     elif len(possibleAnswers) == 1:
-        return possibleAnswers[0]
+        translatedAnswer = g_translate(possibleAnswers[0],detectedLanguageSource)
+        return translatedAnswer
     else:
         idk = random.randint(0, len(possibleAnswers)-1)
-        return possibleAnswers[idk]
+        translatedAnswer = g_translate(possibleAnswers[idk],detectedLanguageSource)
+        return translatedAnswer.strip()
 
     #return ""
